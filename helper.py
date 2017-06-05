7cb3ccfafacc6f9d0d82b2971c31b60e6df6d517
Dhruvraj Rawat <rawat.dhruv14@gmail.com>
2017-06-05 21:21:09 +0530

    Added Trained Model

--- helper.py
+++ helper.py
@@ -1,68 +1,97 @@
-## Importing the Library's
 import os
 import pickle
 import copy
 import numpy as np
 
+
 CODES = {'<PAD>': 0, '<EOS>': 1, '<UNK>': 2, '<GO>': 3 }
 
+
 def load_data(path):
-	input_file=os.path.join(path)
-	with open(input_file,"r",encoding='utf-8',errors='ignore') as f:
-		data=f.read()
-	return data
+    """
+    Load Dataset from File
+    """
+    input_file = os.path.join(path)
+    with open(input_file, "r") as f:
+        data = f.read()
 
-def preprocess_and_save_data(source_path,target_path,text_to_ids):
-	source_text=load_data(source_path)
-	target_text=load_data(target_path)
+    return data
 
-	source_text=source_text.lower()
-	target_text=target_text.lower()
 
-	source_vocab_to_int,source_int_to_vocab=create_lookup_tables(source_text)
-	target_vocab_to_int,target_int_to_vocab=create_lookup_tables(target_text)
+def preprocess_and_save_data(source_path, target_path, text_to_ids):
+    """
+    Preprocess Text Data.  Save to to file.
+    """
+    # Preprocess
+    source_text = load_data(source_path)
+    target_text = load_data(target_path)
 
-	source_text,target_text = text_to_ids(source_text,target_text,source_vocab_to_int,target_vocab_to_int)
+    source_text = source_text.lower()
+    target_text = target_text.lower()
 
-	with open('preprocess.p','wb') as out_file:
-		pickle.dump((
-			(source_text,target_text),
-			(source_vocab_to_int,target_vocab_to_int),
-			(source_int_to_vocab,target_vocab_to_int)),out_file)
+    source_vocab_to_int, source_int_to_vocab = create_lookup_tables(source_text)
+    target_vocab_to_int, target_int_to_vocab = create_lookup_tables(target_text)
+
+    source_text, target_text = text_to_ids(source_text, target_text, source_vocab_to_int, target_vocab_to_int)
+
+    # Save Data
+    pickle.dump((
+        (source_text, target_text),
+        (source_vocab_to_int, target_vocab_to_int),
+        (source_int_to_vocab, target_int_to_vocab)), open('preprocess.p', 'wb'))
+
 
 def load_preprocess():
-	with open('preprocess.p',mode='rb') as in_file:
-		return pickle.load(in_file)
+    """
+    Load the Preprocessed Training data and return them in batches of <batch_size> or less
+    """
+    return pickle.load(open('preprocess.p', mode='rb'))
+
 
 def create_lookup_tables(text):
-	vocab=set(text.split())
-	vocab_to_int=copy.copy(CODES)
+    """
+    Create lookup tables for vocabulary
+    """
+    vocab = set(text.split())
+    vocab_to_int = copy.copy(CODES)
 
-	for v_i, v in enumerate(vocab,len(CODES)):
-		vocab_to_int[v]=v_i
+    for v_i, v in enumerate(vocab, len(CODES)):
+        vocab_to_int[v] = v_i
 
-	int_to_vocab={v_i: v for v,v_i in vocab_to_int.items()}
+    int_to_vocab = {v_i: v for v, v_i in vocab_to_int.items()}
 
-	return vocab_to_int,int_to_vocab
+    return vocab_to_int, int_to_vocab
+
 
 def save_params(params):
+    """
+    Save parameters to file
+    """
+    pickle.dump(params, open('params.p', 'wb'))
 
-	with open('params.p','wb') as out_file:
-		pickle.dump(params,out_file)
 
 def load_params():
-	with open('params.p',mode='rb') as in_file:
-		return pickle.load(in_file)
+    """
+    Load parameters from file
+    """
+    return pickle.load(open('params.p', mode='rb'))
 
-def batch_data(source,target,batch_size):
 
-	for batch_i in range(0,len(source)//batch_size):
-		start_i = batch_i*batch_size
-		source_batch = source[start_i:start_i+batch_size]
-		target_batch = target[start_i:start_i+batch_size]
+def batch_data(source, target, batch_size):
+    """
+    Batch source and target together
+    """
+    for batch_i in range(0, len(source)//batch_size):
+        start_i = batch_i * batch_size
+        source_batch = source[start_i:start_i + batch_size]
+        target_batch = target[start_i:start_i + batch_size]
+        yield np.array(pad_sentence_batch(source_batch)), np.array(pad_sentence_batch(target_batch))
 
-		yield np.array(pad_sentence_batch(source_batch)) , np.array(pad_sentence_batch(target_batch))
 
 def pad_sentence_batch(sentence_batch):
-	max_sentence=max([len(sentence) for sentence in sentence_batch])
-	return [sentence + [CODES['<PAD>']]*(max_sentence-len(sentence)) for sentence in sentence_batch]
+    """
+    Pad sentence with <PAD> id
+    """
+    max_sentence = max([len(sentence) for sentence in sentence_batch])
+    return [sentence + [CODES['<PAD>']] * (max_sentence - len(sentence))
+            for sentence in sentence_batch]
